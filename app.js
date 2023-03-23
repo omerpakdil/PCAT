4ccee479fb516c03511722cd4808f943db054b25
Omer Salih Pakdil <omerp@mobiliz.com.tr>
2023-03-23 16:01:58 +0300

    static files

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const fn = () => {
-  console.log('Hello World');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
 
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`PCAT app listening at http://localhost:${port}`);
+});
